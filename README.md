1af0204db257720dc29f738e5ed9d7b6b15e5827
MANALI TANNA <manalitanna29@gmail.com>
2023-10-07 16:44:57 -0400

    Added optimal solution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NYU-LeetCode-Bootcamp
 Solutions for Homework Assignments for the NYU Tandon leetcode bootcamp
+
+Week 1: Lists, Arrays & Sorting
--- Week 1/ContainerWithMostWater.py
+++ Week 1/ContainerWithMostWater.py
@@ -7,6 +7,21 @@ class Solution(object):
         left_ptr = 0
         right_ptr = len(height)-1
         max_area = 0
+        while (left_ptr < right_ptr):
+            if(height[left_ptr] < height[right_ptr]):
+                area = height[left_ptr] * (right_ptr - left_ptr)
+                left_ptr = left_ptr + 1
+            else:
+                area = height[right_ptr] * (right_ptr - left_ptr)
+                right_ptr = right_ptr - 1
+            max_area = max(area, max_area)
+        return max_area
+    
+        """
+        Brute force - time limit exceeded
+        left_ptr = 0
+        right_ptr = len(height)-1
+        max_area = 0
         for left_ptr in range(0,len(height)):
             for right_ptr in range (left_ptr + 1, len(height)):
                 if(height[left_ptr] < height[right_ptr]):
@@ -16,4 +31,4 @@ class Solution(object):
                 if area > max_area:
                     max_area = area
         return max_area
-        
+        """
