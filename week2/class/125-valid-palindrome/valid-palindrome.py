613c6f7073f70ee45a6f363e14838c03e0df8d72
ManaliTanna <manalitanna29@gmail.com>
2023-10-11 14:42:33 -0400

    Time: 32 ms (55.69%) | Memory: 15.3 MB (35.44%) - LeetSync

--- week2/class/125-valid-palindrome/valid-palindrome.py
+++ week2/class/125-valid-palindrome/valid-palindrome.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        lptr = 0
-        rptr = len(s)-1
         s = s.lower()
-        s = s.replace(" ","")
-        s = ''.join(filter(lambda x: x.isalpha() or x.isdigit() or x.isspace(), s))
+        s = ''.join(filter(lambda x: x.isalnum(), s))
         if (s[::-1] == s):
             return True
         else:
