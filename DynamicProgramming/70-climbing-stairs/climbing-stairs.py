7a2342c3d38581bb929c98fc9d2f3e9d653f146a
ManaliTanna <manalitanna29@gmail.com>
2024-02-19 13:36:13 -0500

    Time: 11 ms (79.29%) | Memory: 11.8 MB (66.65%) - LeetSync

---
+++ DynamicProgramming/70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ways_taking_one_step = 1
+        way_taking_two_step = 1
+        
+        for i in range(n-1):
+
+            temp = ways_taking_one_step
+
+            #Caluclate total no. of ways the next 
+            ways_taking_one_step = ways_taking_one_step + way_taking_two_step
+
+            #update for next iteration
+            way_taking_two_step = temp
+
+        return ways_taking_one_step
+
+
+        
