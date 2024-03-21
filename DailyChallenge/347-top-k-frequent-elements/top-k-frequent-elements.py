e426322a7064cdafadbb40108f7a922b6d16d173
ManaliTanna <manalitanna29@gmail.com>
2024-03-21 15:53:14 -0400

    Time: 84 ms (24.34%) | Memory: 19.4 MB (9.15%) - LeetSync

--- DailyChallenge/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ DailyChallenge/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,15 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # General Algorithm for top K --> even in big data
+        # Find frequency
+        # Keep counts as index
+        # Then find top k based on the counts
+
         frequency = {}
-        for num in nums:
-            if num in frequency:
-                frequency[num] += 1
-            else:
-                frequency[num] = 1
+        counts = [[] for i in range (len(nums) + 1)]
 
-        # Sorting the dictionary items by frequency
-        sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+        # create a frequency hashmap
+        for i in nums:
+            frequency[i] = 1 + frequency.get(i,0)
 
-        # Extracting the k most frequent elements
-        return [element[0] for element in sorted_frequency[:k]]
+        # create array with count as index
+        for num, count in frequency.items():
+            counts[count].append(num)
+
+        result = []
+
+        # Traverse counts array and find top k
+        for num_list in range(len(counts) - 1 , 0 , -1):
+            for each_num in counts[num_list]:
+                result.append(each_num)
+                if len(result) == k:
+                    return result
