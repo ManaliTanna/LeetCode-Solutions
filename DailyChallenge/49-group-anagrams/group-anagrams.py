098f2a48c8a5a5f45e31ba04d1162657f36426ad
ManaliTanna <manalitanna29@gmail.com>
2024-03-21 15:09:59 -0400

    Time: 69 ms (69.88%) | Memory: 15.6 MB (79.49%) - LeetSync

--- DailyChallenge/49-group-anagrams/group-anagrams.py
+++ DailyChallenge/49-group-anagrams/group-anagrams.py
@@ -5,19 +5,15 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         anagrams = {}
-    
+
         for word in strs:
-            # Sort the characters of the word to create a unique key for anagrams
+
             sorted_word = ''.join(sorted(word))
-        
-            # Check if the sorted_word is already in the hash table
+
             if sorted_word in anagrams:
                 anagrams[sorted_word].append(word)
             else:
                 anagrams[sorted_word] = [word]
-    
-        # Convert the values of the hash table to a list to get the final result
-        result = list(anagrams.values())
-    
-        return result
+        
+        return list(anagrams.values())
         
