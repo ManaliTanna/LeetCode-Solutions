6c35afee45cb155cb805b061af1cbccf6ca389ad
ManaliTanna <manalitanna29@gmail.com>
2025-02-10 23:12:56 -0500

    Time: 49 ms (61.51%) | Memory: 33.3 MB (76.34%) - LeetSync

--- DSA/128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ DSA/128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,36 +1,26 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
+        #set of all nums O(1)
+        my_set = set(nums)
 
-        for num in numSet:
-            if (num - 1) not in numSet:
-                length = 1
-                while (num + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
-        # #set of all nums O(1)
-        # my_set = set(nums)
+        #final answer
+        count = 0
 
-        # #final answer
-        # count = 0
+        # iterate O(n)
+        for i in my_set:
+            # if i-1 does not exist it could potentially be the start of a sequence
+            if i-1 not in my_set:
 
-        # # iterate O(n)
-        # for i in nums:
-        #     # if i-1 does not exist it could potentially be the start of a sequence
-        #     if i-1 not in my_set:
+                #start counting length of sequence
+                seq_len = 0
+                # as long as starting number + length exists in set, increment length
+                while (i+seq_len) in my_set:
+                    seq_len = seq_len + 1
 
-        #         #start counting length of sequence
-        #         seq_len = 0
-        #         # as long as starting number + length exists in set, increment length
-        #         while (i+seq_len) in my_set:
-        #             seq_len = seq_len + 1
+                #consider max of current count and sequence length and set the new count
+                count = max(count,seq_len)
 
-        #         #consider max of current count and sequence length and set the new count
-        #         count = max(count,seq_len)
-
-        # return count
+        return count
 
 
             
