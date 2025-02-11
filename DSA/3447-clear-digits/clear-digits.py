3bf25bfe755f79ee2dc69db0950952b5ed6de169
ManaliTanna <manalitanna29@gmail.com>
2025-02-11 13:32:47 -0500

    Time: 2 ms (54.29%) | Memory: 12.5 MB (10.61%) - LeetSync

--- DSA/3447-clear-digits/clear-digits.py
+++ DSA/3447-clear-digits/clear-digits.py
@@ -4,22 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        def isdigit(x):
-            if x>0 and x<9:
-                return True
         stack = []
-        top_of_stack = 0
         for char in s:
-            # print("char: ",char)
             if char.isdigit():
-                # print("top: ",top_of_stack)
-                top_of_stack = top_of_stack - 1
                 stack.pop()
             else:
-                top_of_stack = top_of_stack + 1
-                # print("top: ",top_of_stack)
                 stack.append(char)
-        # print(stack)
         return "".join(stack)
 
 
