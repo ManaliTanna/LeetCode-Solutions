99cd37b65e4302eb8d4282beb6d00e30f2ad0cf0
ManaliTanna <manalitanna29@gmail.com>
2025-02-25 13:54:51 -0500

    Time: 11 ms (36.72%) | Memory: 18.1 MB (69.67%) - LeetSync

--- DSA/125-valid-palindrome/valid-palindrome.py
+++ DSA/125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,40 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # function to determine if a character is non alphanumeric
+        def is_non_alphanumeric(c):
+            if c in '0123456789':
+                return False
+            elif c in 'abcdefghijklmnopqrstuvwxyz':
+                return False
+            else:
+                return True
+
+        # pointer to track characters starting from the left
+        l_ptr = 0
+
+        # pointer to track characters starting from the right
+        r_ptr = len(s)-1
+
+        #convert string to lowercase
         s = s.lower()
-        s = ''.join(filter(lambda x: x.isalnum(), s))
-        if (s[::-1] == s):
-            return True
-        else:
-            return False
+
+        # until pointers dont exceed bound
+        while s and r_ptr>0 and l_ptr<len(s) and l_ptr < r_ptr:
+
+            #ignore character if non alphanumeric
+            while s and is_non_alphanumeric(s[l_ptr]) and l_ptr < r_ptr:
+                l_ptr = l_ptr + 1
+            while s and is_non_alphanumeric(s[r_ptr]) and l_ptr < r_ptr:
+                r_ptr = r_ptr - 1
+
+            # if characters on both side are not equal return False
+            if s[l_ptr] != s[r_ptr]:
+                return False
+            
+            # Move the pointer in correct direction
+            l_ptr = l_ptr + 1
+            r_ptr = r_ptr - 1
+        
+        return True
+
