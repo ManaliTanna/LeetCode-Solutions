2462b0ae6f9014372ed45a7dbd72644aec03c88b
ManaliTanna <manalitanna29@gmail.com>
2025-03-01 17:26:10 -0500

    Time: 79 ms (70.22%) | Memory: 49.8 MB (89.87%) - LeetSync

--- DSA/435-non-overlapping-intervals/non-overlapping-intervals.py
+++ DSA/435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -7,23 +7,15 @@ class Solution:
         # sort based on finishing time of all intervals
         intervals = sorted(intervals, key=lambda x: x[1])
 
-        # print(intervals)
-
-        # add first pair to result array
-        # result = [intervals[0]]
-
         #keep track of finishing time of pair recently added to result
         finishing_time = intervals[0][1]
 
         count = 0
 
-        # print(finishing_time)
-
         # iterate through array
         for i in range(1,n):
 
             # if the next element starts after the previously added element ends
-            # add it to the result array
             # and keep track of its finishing time
             if intervals[i][0] >= finishing_time:
                 # result.append(intervals[i])
@@ -31,8 +23,6 @@ class Solution:
             else:
                 count = count + 1
         
-        # print(result)
         return count
-        # return n-len(result)
 
 
